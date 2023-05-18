0b26d91c058dc7cda0f8f1bdeba2721f608b7863
eiximenis <etomas@gmail.com>
2023-05-18 19:47:26 +0200

    added sample unoplatform apps

---
+++ code/ArcSanMarti/ArcSanMarti.Skia.Linux.FrameBuffer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Uno.UI.Runtime.Skia;
+
+namespace ArcSanMarti
+{
+	public sealed class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.CursorVisible = false;
+
+				var host = new FrameBufferHost(() => new App());
+				host.Run();
+			}
+			finally
+			{
+				Console.CursorVisible = true;
+			}
+		}
+	}
+}
